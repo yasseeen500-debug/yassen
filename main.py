697849d5d2921f416253048b94d6d950686f7234
yasseeen500-debug <yasseeen500@gmail.com>
2026-07-27 00:57:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+print("------WELCOME TO CALCULATOR SAIR------\n")
+#valubles 1
+num1=float(input("First number="))
+num2=float(input("Second number="))
+#valubles 2
+Sum=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+mod=num1%num2
+#printing
+print("|--------------------|")
+print("Adding="  +str(Sum))
+print("Subtract="+str(sub))
+print("Multiply="+str(mul))
+print("Divide="  +str(div))
+print("Modulus=" +str(mod))
